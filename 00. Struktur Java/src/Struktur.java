8a9a1826dea10f3b9e0fd2c6c836b69150f31058
HamsterKaget <radzaramdani21@gmail.com>
2022-02-20 15:27:51 +0700

    Stuktur Penulisan Java

---
+++ 00. Struktur Java/src/Struktur.java
@@ -0,0 +1,24 @@
+/**
+ * Program Hello-World
+ * Dalam Java , Code harus diawali dengan mendeklarasikan class terlebih dahulu
+ * Untuk mendeklarasikan class dalam Java bisa menggunakan `class <namaClass> {}`
+ * Note : * nama class dalam java harus sama dengan nama file tersebut !
+ *        * pembahasan tentang 'public' akan di bahas di Java OOP , untuk sekarang ikuti penulisan seperti dibawah!
+ */
+public class Struktur {
+
+    /**
+     * Dalam Java, Code harus dituliskan di dalam suatu method
+     * untuk mendeklarasikan method kita bisa menggunakan `public static void <namaMethod>(argumen) {}`
+     * Note : * Dalam java kita bisa menggunakan lebih dari satu method namun wajib memiliki satu method utama yaitu main
+     * * Pembahasan mengenai Method akan di bahas di chapter yang akan datang
+     */
+    public static void main(String[] args) {
+        // code ditulis di dalam method main
+        System.out.println("Hello World !"); // <- di akhir setiap statement jangan lupa titik koma
+
+
+
+    }
+
+}
