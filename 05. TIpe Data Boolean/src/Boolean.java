5d410ee4379fe7f77383cd596c887a86b3663a84
HamsterKaget <radzaramdani21@gmail.com>
2022-02-20 21:01:33 +0700

    Tipe Data Boolean Java

---
+++ 05. TIpe Data Boolean/src/Boolean.java
@@ -0,0 +1,30 @@
+/** Tipe Data boolean
+ * Dalam Java, boolean merupakan tipe data primitive yang berfungsi untuk menyimpan hanya 2 values saja
+ * `true` dan `false` , boolean memiliki storage size sebanyak 2 byte atau 16 bit,
+ * Umumnya boolean terintegrasi dengan operator kondisi ( conditional statement )
+ */
+
+public class Boolean {
+
+    public static void main(String[] args) {
+
+        /** Penulisan Boolean
+         * Untuk menuliskan boolean di java kita menggunakan keyword `boolean <namaVariabel> = <true/false>`
+         */
+
+        /** Boolean ( Menyimpan Dua Nilai `True` / `False` ) {
+         *      - Default Value : false
+         *      - Size Memory : 2 byte / 16 bit
+         *      - Menyimpan true atau false saja
+         * }
+         */
+        boolean isContinue = true;
+
+        System.out.println(isContinue);
+
+
+
+
+    }
+
+}
